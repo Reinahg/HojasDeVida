b318a1d4807c2360a2653463376ffad36b3599d5
Valeria <valeria.henao@hotmail.com>
2022-06-02 21:09:38 -0500

    filtros

--- Formularios/Inicio.Designer.cs
+++ Formularios/Inicio.Designer.cs
@@ -40,6 +40,10 @@
             this.label4 = new System.Windows.Forms.Label();
             this.tfExp = new System.Windows.Forms.TextBox();
             this.searchExp = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tfProfesion = new System.Windows.Forms.TextBox();
+            this.btnProfesion = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.tableData)).BeginInit();
             this.SuspendLayout();
             // 
@@ -47,7 +51,7 @@
             // 
             this.NewHoja.BackColor = System.Drawing.Color.Aqua;
             this.NewHoja.Font = new System.Drawing.Font("Rockwell", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.NewHoja.Location = new System.Drawing.Point(732, 12);
+            this.NewHoja.Location = new System.Drawing.Point(824, 12);
             this.NewHoja.Name = "NewHoja";
             this.NewHoja.Size = new System.Drawing.Size(234, 43);
             this.NewHoja.TabIndex = 0;
@@ -65,14 +69,14 @@
             this.tableData.ReadOnly = true;
             this.tableData.RowHeadersWidth = 51;
             this.tableData.RowTemplate.Height = 29;
-            this.tableData.Size = new System.Drawing.Size(617, 370);
+            this.tableData.Size = new System.Drawing.Size(726, 388);
             this.tableData.TabIndex = 1;
             // 
             // searchName
             // 
             this.searchName.BackColor = System.Drawing.Color.NavajoWhite;
             this.searchName.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.searchName.Location = new System.Drawing.Point(782, 162);
+            this.searchName.Location = new System.Drawing.Point(891, 162);
             this.searchName.Name = "searchName";
             this.searchName.Size = new System.Drawing.Size(94, 29);
             this.searchName.TabIndex = 2;
@@ -93,7 +97,7 @@
             // tfName
             // 
             this.tfName.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.tfName.Location = new System.Drawing.Point(651, 162);
+            this.tfName.Location = new System.Drawing.Point(760, 162);
             this.tfName.Name = "tfName";
             this.tfName.Size = new System.Drawing.Size(125, 31);
             this.tfName.TabIndex = 4;
@@ -102,7 +106,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(651, 126);
+            this.label2.Location = new System.Drawing.Point(760, 126);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(184, 22);
             this.label2.TabIndex = 5;
@@ -112,7 +116,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(651, 257);
+            this.label3.Location = new System.Drawing.Point(760, 208);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(161, 22);
             this.label3.TabIndex = 8;
@@ -121,7 +125,7 @@
             // tfYears
             // 
             this.tfYears.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.tfYears.Location = new System.Drawing.Point(651, 293);
+            this.tfYears.Location = new System.Drawing.Point(760, 244);
             this.tfYears.Name = "tfYears";
             this.tfYears.Size = new System.Drawing.Size(125, 31);
             this.tfYears.TabIndex = 7;
@@ -130,7 +134,7 @@
             // 
             this.searchYearsOld.BackColor = System.Drawing.Color.NavajoWhite;
             this.searchYearsOld.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.searchYearsOld.Location = new System.Drawing.Point(782, 293);
+            this.searchYearsOld.Location = new System.Drawing.Point(891, 244);
             this.searchYearsOld.Name = "searchYearsOld";
             this.searchYearsOld.Size = new System.Drawing.Size(94, 29);
             this.searchYearsOld.TabIndex = 6;
@@ -142,7 +146,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label4.Location = new System.Drawing.Point(651, 383);
+            this.label4.Location = new System.Drawing.Point(760, 292);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(298, 22);
             this.label4.TabIndex = 11;
@@ -151,17 +155,16 @@
             // tfExp
             // 
             this.tfExp.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.tfExp.Location = new System.Drawing.Point(651, 419);
+            this.tfExp.Location = new System.Drawing.Point(760, 328);
             this.tfExp.Name = "tfExp";
             this.tfExp.Size = new System.Drawing.Size(125, 31);
             this.tfExp.TabIndex = 10;
-            this.tfExp.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // searchExp
             // 
             this.searchExp.BackColor = System.Drawing.Color.NavajoWhite;
             this.searchExp.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.searchExp.Location = new System.Drawing.Point(782, 419);
+            this.searchExp.Location = new System.Drawing.Point(891, 328);
             this.searchExp.Name = "searchExp";
             this.searchExp.Size = new System.Drawing.Size(94, 29);
             this.searchExp.TabIndex = 9;
@@ -169,11 +172,57 @@
             this.searchExp.UseVisualStyleBackColor = false;
             this.searchExp.Click += new System.EventHandler(this.searchExp_Click);
             // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.NavajoWhite;
+            this.button1.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(760, 465);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(225, 49);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Limpiar filtros";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(760, 377);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(199, 22);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Buscar por profesion";
+            // 
+            // tfProfesion
+            // 
+            this.tfProfesion.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tfProfesion.Location = new System.Drawing.Point(760, 413);
+            this.tfProfesion.Name = "tfProfesion";
+            this.tfProfesion.Size = new System.Drawing.Size(125, 31);
+            this.tfProfesion.TabIndex = 14;
+            // 
+            // btnProfesion
+            // 
+            this.btnProfesion.BackColor = System.Drawing.Color.NavajoWhite;
+            this.btnProfesion.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnProfesion.Location = new System.Drawing.Point(891, 413);
+            this.btnProfesion.Name = "btnProfesion";
+            this.btnProfesion.Size = new System.Drawing.Size(94, 29);
+            this.btnProfesion.TabIndex = 13;
+            this.btnProfesion.Text = "Buscar";
+            this.btnProfesion.UseVisualStyleBackColor = false;
+            this.btnProfesion.Click += new System.EventHandler(this.profesion_click);
+            // 
             // Inicio
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(978, 508);
+            this.ClientSize = new System.Drawing.Size(1065, 543);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tfProfesion);
+            this.Controls.Add(this.btnProfesion);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.tfExp);
             this.Controls.Add(this.searchExp);
@@ -188,6 +237,7 @@
             this.Controls.Add(this.NewHoja);
             this.Name = "Inicio";
             this.Text = "Inicio";
+            this.Load += new System.EventHandler(this.Inicio_Load);
             ((System.ComponentModel.ISupportInitialize)(this.tableData)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -208,5 +258,9 @@
         private Label label4;
         private TextBox tfExp;
         private Button searchExp;
+        private Button button1;
+        private Label label5;
+        private TextBox tfProfesion;
+        private Button btnProfesion;
     }
 }
--- Formularios/Inicio.cs
+++ Formularios/Inicio.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using System.Collections;
 using HojasDeVida.Clases;
 
 namespace HojasDeVida.Formularios
@@ -20,6 +21,8 @@ namespace HojasDeVida.Formularios
         SqlDataAdapter da;
         DataTable dt;
 
+        DataTable dtHojasVida = new DataTable();
+
         public Inicio()
         {
             InitializeComponent();
@@ -28,6 +31,7 @@ namespace HojasDeVida.Formularios
 
         private void NewHoja_Click(object sender, EventArgs e)
         {
+            this.Hide();
             HojaVida hoja = new HojaVida(true);//0 para crear, 1 para leer
             
             hoja.Show();
@@ -35,31 +39,88 @@ namespace HojasDeVida.Formularios
 
         private void configureData()
         {
-            cmd = new SqlCommand("select * from HojaDeVida ", cn.AbrirConexion());
+
+            dtHojasVida.Columns.Add("Cedula", typeof(string));
+            dtHojasVida.Columns.Add("Nombre", typeof(string));
+            dtHojasVida.Columns.Add("Edad", typeof(string));
+            dtHojasVida.Columns.Add("Profesión", typeof(string));
+            dtHojasVida.Columns.Add("AñosExperiencia", typeof(string));
+
+            cmd = new SqlCommand("select HojaDeVida.Cedula, HojaDeVida.Nombre, HojaDeVida.Edad, HojaDeVida.Profesion, Experiencia.AnosExp from HojaDeVida inner join Experiencia on HojaDeVida.Experiencia = Experiencia.IdExperiencia", cn.AbrirConexion());
             da = new SqlDataAdapter(cmd);
             dt = new DataTable();
             da.Fill(dt);
 
-        }
 
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
+            foreach (DataRow row in dt.Rows)
+            {
+                string cedula = row["Cedula"].ToString();
+                string nombre = row["Nombre"].ToString();
+                string edad = row["Edad"].ToString();
+                string profesion = row["Profesion"].ToString();
+                string years = row["AnosExp"].ToString();
+
+
+                dtHojasVida.Rows.Add(new object[] { cedula,nombre,edad,profesion,years });
+            }
+
+            tableData.DataSource = dtHojasVida;
+
 
         }
 
         private void searchExp_Click(object sender, EventArgs e)
         {
-
+            DataView dv = dtHojasVida.DefaultView;
+            dv.RowFilter = "AñosExperiencia LIKE '" + tfExp.Text + "%'";
+            tableData.DataSource = dv;
         }
 
         private void searchYearsOld_Click(object sender, EventArgs e)
         {
+            
+            DataView dv = dtHojasVida.DefaultView;
+            dv.RowFilter = "Edad LIKE '" + tfYears.Text + "%'";
+            tableData.DataSource = dv;
 
         }
 
         private void searchName_Click(object sender, EventArgs e)
         {
+            DataView dv = dtHojasVida.DefaultView;
+            dv.RowFilter = "Nombre LIKE '" + tfName.Text + "%'";
+            tableData.DataSource = dv;
+        }
+
+        private void profesion_click(object sender, EventArgs e)
+        {
+            DataView dv = dtHojasVida.DefaultView;
+            dv.RowFilter = "Profesión LIKE '" + tfProfesion.Text + "%'";
+            tableData.DataSource = dv;
 
         }
+
+        private void Inicio_Load(object sender, EventArgs e)
+        {
+            configureData();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DataView dv = dtHojasVida.DefaultView;
+            dv.RowFilter = "Edad LIKE '%'";
+            dv.RowFilter = "Profesión LIKE '%'";
+            dv.RowFilter = "Nombre LIKE '%'";
+            dv.RowFilter = "AñosExperiencia LIKE '%'";
+
+            tableData.DataSource = dv;
+
+            tfName.Text = "";
+            tfExp.Text = "";
+            tfProfesion.Text = "";
+            tfYears.Text = "";
+        }
+
+        
     }
 }
